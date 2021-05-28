65ac87fa398492ca61d82ae3ac29d3dbd4c5b974
Rafael Senties Martinelli <rafael@senties-martinelli.com>
2021-05-28 22:45:19 +0200

    Allowd the use of AKBL without the AppIndicator

--- usr/lib/python3/AKBL/Addons/gtk_utils.py
+++ usr/lib/python3/AKBL/Addons/gtk_utils.py
@@ -18,7 +18,12 @@
 
 import gi
 gi.require_version('Gtk', '3.0')
-gi.require_version('AppIndicator3', '0.1')
+
+try:
+    gi.require_version('AppIndicator3', '0.1')
+except:
+    print("Warning: Running without the AppIndicator. The dependency is not installed.")
+
 from gi.repository import Gtk
 
 def get_text_gtk_buffer(textbuffer):
