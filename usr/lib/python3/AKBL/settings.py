fcae145a87e7f8288558d3999ffcddf6dfc0efae
Rafael Senties Martinelli <rafael@senties-martinelli.com>
2024-01-28 23:21:33 +0100

    Debug mode, with env variable DEBUG=true

--- usr/lib/python3/AKBL/settings.py
+++ usr/lib/python3/AKBL/settings.py
@@ -16,5 +16,11 @@
 #   along with this program; if not, write to the Free Software Foundation,
 #   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-__version__ = "2019.05.26-rev2024"
-DEBUG = True
+import os
+
+__version__ = "2.dev"
+
+if 'DEBUG' in os.environ:
+    DEBUG = os.environ['DEBUG'].lower() == 'true'
+else:
+    DEBUG = False
