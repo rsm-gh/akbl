2643c29d10ccacf100861e5307a271b86bac972a
Rafael Senties Martinelli <rafael@senties-martinelli.com>
2024-08-04 13:21:47 +0200

    GUI: display daemon status

--- usr/lib/python3/AKBL/settings.py
+++ usr/lib/python3/AKBL/settings.py
@@ -16,7 +16,7 @@
 #   along with this program; if not, write to the Free Software Foundation,
 #   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-__version__ = "2.240804~2"
+__version__ = "2.240804~3"
 
 
 class IndicatorCodes:
